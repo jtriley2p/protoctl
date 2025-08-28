f0ac145c3d82513db5114f44fc82823032419ac3
jtriley <jtriley15@gmail.com>
2025-08-28 18:22:50 -0500

    add placeholder test so gh leaves me alone ill add tests soon

---
+++ src/test/Placeholder.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
+
+import {Test} from "lib/forge-std/src/Test.sol";
+
+contract Placeholder is Test {
+    function testNothing() public {}
+}
